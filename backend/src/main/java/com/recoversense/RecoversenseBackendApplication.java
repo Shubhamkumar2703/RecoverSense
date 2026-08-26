9ce0ae281b90ac7da9dbc18cad886695233565cb
Shubhamkumar2703 <shubham27034@gmail.com>
2026-08-27 02:47:35 +0530

    chore: establish backend runtime infrastructure

---
+++ backend/src/main/java/com/recoversense/RecoversenseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.recoversense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecoversenseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecoversenseBackendApplication.class, args);
+	}
+
+}
